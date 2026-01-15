b169d3ed56277205cbe96414cbd101e5dbcf8cbe
emoriver <emoriver@live.it>
2026-01-15 15:28:57 +0100

    sistemata assegnazione utenti a host sul flake

--- flake.nix
+++ flake.nix
@@ -1,88 +1,3 @@
-/*
-{
-  description = "NixOS + Home Manager multi-host compatto";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, flake-utils, ... }:
-  let
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-
-    # pkgs stable
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
-    # pkgs unstable (li passiamo ai moduli Home)
-    pkgsUnstable = 
-      import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-    };
-
-
-    # tabella host → moduli
-    hosts = {
-      macpnixos = {
-        hostModule = ./hosts/macpnixos;
-        homeModule = ./home/emoriver/macpnixos.nix;
-      };
-      w541onnixos = {
-        hostModule = ./hosts/w541onnixos;
-        homeModule = ./home/emoriver/w541onnixos.nix;
-      };
-      nix-immich-70 = {
-        hostModule = ./hosts/nix-immich-70;
-        homeModule = ./home/emoriver/nix-immich-70.nix;
-      };
-      t4801onnixos = {
-        hostModule = ./hosts/t4801onnixos;
-        homeModule = ./home/carpinera/t4801onnixos.nix;
-      };      
-    };
-
-    mkNixos = name: cfg:
-      lib.nixosSystem {
-        inherit system;
-        modules = [
-          cfg.hostModule
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.extraSpecialArgs = { inherit pkgsUnstable; };
-            home-manager.users.emoriver = import cfg.homeModule;
-            home-manager.users.carpinera = import cfg.homeModule;
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
-        # opzionale: se ti serve usare pkgs/pkgsUnstable anche nei moduli NixOS
-        specialArgs = { inherit pkgsUnstable; };
-      };
-  in
-  {
-    # ⬇⬇⬇ Top-level, come si aspetta nixos-rebuild
-    nixosConfigurations = lib.mapAttrs mkNixos hosts;
-
-    # (opzionale) Se vuoi generare pacchetti multi‑arch con flake-utils:
-    # packages = flake-utils.lib.eachDefaultSystem (system:
-    #   let pkgsFor = import nixpkgs { inherit system; };
-    #   in { default = pkgsFor.hello; }
-    # ).packages;
-  };
-}
-*/
-
-
 {
   description = "NixOS + Home Manager multi-host multi-user";
 
@@ -106,15 +21,26 @@
       config.allowUnfree = true;
     };
 
-    # Definiamo i moduli home per utente per ogni host
     hosts = {
       macpnixos = {
         hostModule = ./hosts/macpnixos;
         users = {
           emoriver = ./home/emoriver/macpnixos.nix;
-          #carpinera = ./home/carpinera/macpnixos.nix; # Aggiungi se necessario
         };
       };
+      nix-immich-70 = {
+        hostModule = ./hosts/nix-immich-70;
+        users = {
+          emoriver = ./home/emoriver/nix-immich-70.nix;
+        };
+      };
+      w541onnixos = {
+        hostModule = ./hosts/w541onnixos;
+        users = {
+          #carpinera = ./home/carpinera/w541onnixos.nix;
+          emoriver = ./home/emoriver/w541onnixos.nix;
+        };
+      };      
       t4801onnixos = {
         hostModule = ./hosts/t4801onnixos;
         users = {
@@ -122,9 +48,6 @@
           #emoriver = ./home/emoriver/t4801onnixos.nix;
         };
       };
-      # ... aggiungi gli altri host seguendo questo schema
-      #w541
-      #nix-immich
     };
 
     mkNixos = name: cfg:
@@ -139,8 +62,9 @@
               useGlobalPkgs = true;
               useUserPackages = true;
               extraSpecialArgs = { inherit pkgsUnstable; };
-              # Mappiamo dinamicamente gli utenti definiti in 'hosts'
+
               users = lib.mapAttrs (user: path: import path) cfg.users;
+
               backupFileExtension = "backup";
             };
           }
