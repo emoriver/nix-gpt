027933bec393fd803625faef34a1345aee4adfdb
Andrea Riva <emoriver@live.it>
2026-05-06 21:27:23 +0200

    update mac pro

--- hosts/macpnixos/default.nix
+++ hosts/macpnixos/default.nix
@@ -73,8 +73,8 @@
 
   # You might need this if you get insecure package warnings
   nixpkgs.config.permittedInsecurePackages = [
-    #"broadcom-sta-6.30.223.271-59-6.12.77"
-    "broadcom-sta-6.30.223.271-59-6.12.81"
+    #"broadcom-sta-6.30.223.271-59-6.12.81"
+    "broadcom-sta-6.30.223.271-59-6.12.85"
   ];
 
 
