66572c001a1bf225e14405e13abecaf03ad6754f
Andrea Riva <emoriver@live.it>
2026-05-10 14:38:52 +0200

    -

--- modules/nixos/services/soulseek.nix
+++ modules/nixos/services/soulseek.nix
@@ -1,24 +1,30 @@
 {
-    services.slskd = {
-        enable = true;
-        openFirewall = true; # Apre le porte per i download e l'interfaccia web
-        domain = "192.168.88.192";
-        environmentFile = "/etc/slskd/credentials.env";
-        settings = {
-            directories = {
-                # Dove scaricherai la musica (magari sulla tua nuova chiavetta USB!)
-                downloads = "/mnt/usb_hp_musica/usb_k2/musica/downloads";
-                incomplete = "/mnt/usb_hp_musica/usb_k2/musica/downloads/incomplete";
-            };
-            sharing = {
-                directories = [ "/mnt/usb_hp_musica/usb_k2/musica" ]; # Condividi la tua libreria (fondamentale su Soulseek)
-            };
-            ui = {
-                web = {
-                    port = 5030;
-                };
-            };
-        };
-        systemd.services.slskd.serviceConfig.StateDirectory = "slskd";
+  services.slskd = {
+    enable = true;
+    openFirewall = true;
+    domain = null;
+    nginx.enable = false;
+    environmentFile = "/etc/slskd/credentials.env";
+
+    settings = {
+      directories = {
+        downloads = "/mnt/usb_hp_musica/usb_k2/musica/downloads";
+        incomplete = "/mnt/usb_hp_musica/usb_k2/musica/downloads/incomplete";
+      };
+      shares = {
+        directories = [ "/mnt/usb_hp_musica/usb_k2/musica" ];
+      };
     };
+  };
+
+  systemd.services.slskd.serviceConfig = {
+    StateDirectory = "slskd";
+    ProtectHome = pkgs.lib.mkForce "read-only"; 
+    ProtectSystem = pkgs.lib.mkForce "full";
+    ReadWritePaths = [ "/mnt/usb_hp_musica/usb_k2/musica/downloads" ];
+    
+    PrivateDevices = pkgs.lib.mkForce false;
+    RestrictNamespaces = pkgs.lib.mkForce false;
+  };
 }
+
