9411b35d69a5d0c6475702efbdecc113e75dea35
emoriver <emoriver@live.it>
2026-06-12 15:38:42 +0200

    -

--- packages/thingsboard.nix
+++ packages/thingsboard.nix
@@ -4,6 +4,9 @@ pkgs.stdenv.mkDerivation {
   pname = "thingsboard";
   version = "4.3.1.2-wrapper";
 
+  # Force rebuild
+  rev = "20260612-002";
+
   src = pkgs.fetchurl {
     url = "https://github.com/thingsboard/thingsboard/releases/download/v4.3.1.2/thingsboard-4.3.1.2.deb";
     sha256 = "0nryixswpx1z6s3rb0wbc8p17d7yyg6376si17v03czvfa0ivfym";
