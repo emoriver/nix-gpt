67b2cb48861db6376bd6cee4b7dbd8ca9cb6eab9
Andrea Riva <emoriver@live.it>
2026-06-12 11:43:50 +0200

    sistemato VSCodium (!)

--- modules/home/apps/vscodium.nix
+++ modules/home/apps/vscodium.nix
@@ -1,35 +1,15 @@
 {pkgs, ...}:
 
 {
-  programs.vscode = {
+  programs.vscodium = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
       #continue.continue
     ];
-    userSettings = {
+    profiles.default.userSettings = {
       "nix.serverPath" = "nixd";
     };    
   };
-
-/*
-  # Kiro usa ~/.config/Kiro invece di ~/.config/Code,
-  # quindi le settings vanno scritte direttamente lì  
-  home.file.".config/Kiro/User/settings.json" = {
-    force = true;
-    text = builtins.toJSON {
-      "kiroAgent.configureMCP" = "Disabled";
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings" = {
-        "nil" = {
-          "formatting" = {
-            "command" = [ "nixfmt" ];
-          };
-        };
-      };
-    };
-  };
-*/
 }
