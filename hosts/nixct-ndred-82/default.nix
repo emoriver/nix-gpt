801da0193c35d6220b6568bd93ff4119fd83dfaa
emoriver <emoriver@live.it>
2026-06-24 17:07:55 +0200

    provo a configurare node-red su ct proxmox

---
+++ hosts/nixct-ndred-82/default.nix
@@ -0,0 +1,92 @@
+{ config, modulesPath, pkgs, lib, ... }:
+{
+  imports = [ 
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+
+    # pacchetti di sistema
+
+    # utenti - !! -
+    ../../modules/nixos/users/carpinera.nix
+
+    # servizi
+    ../../modules/nixos/services/node-red.nix
+  ];
+  
+  nix.settings = { sandbox = false; };  
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+
+
+  # ----- impostazioni di nix -----
+  nixpkgs.config.allowUnfree = true;
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+    warn-dirty = false;
+    substituters = [
+      "https://cache.nixos.org/"
+      # Aggiungi altri se li usi (es. Cachix)
+    ];
+    # trusted-public-keys = [ ... ];
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+
+  # ----- servizi di base -----
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+      KbdInteractiveAuthentication = false;
+      UsePAM = true;
+      AllowUsers = [ "emoriver" ];
+      PermitEmptyPasswords = "no";
+    };
+  };
+
+  security.pam.services.sshd.allowNullPassword = false;
+
+  enableDocker = true;
+
+
+  # ----- programmi e pacchetti di sistema host-level  -----
+  programs = {
+    zsh.enable = true;
+    #git.enable = true;
+    ssh.startAgent = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    #vim
+    git 
+    curl 
+    wget 
+    htop 
+    #btop 
+    ripgrep 
+    fd 
+    unzip 
+    zip 
+    gnupg 
+    tmux
+    yazi
+  ];
+
+  services.fstrim.enable = false; # Let Proxmox host handle fstrim
+
+  systemd.mounts = [{
+    where = "/sys/kernel/debug";
+    enable = false;
+  }];
+
+  system.stateVersion = "25.11";
+}
