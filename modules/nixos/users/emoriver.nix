f8d9bc8f6b876653f240167cd271ad44374786ad
Andrea Riva <emoriver@live.it>
2026-06-24 20:23:10 +0200

    linger utente per consentire ai servizi di partire senza la login utente

--- modules/nixos/users/emoriver.nix
+++ modules/nixos/users/emoriver.nix
@@ -3,17 +3,26 @@
 {
   users.users.emoriver = {
     isNormalUser = true;
+    linger = true;
     description = "Andrea Riva";
     home = "/home/emoriver";
     createHome = true;
-    extraGroups = [ "wheel" "networkmanager" "audio" "git" "video" "sudo" "docker" ]; # aggiungi "docker" se lo usi
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+      "git"
+      "video"
+      "sudo"
+      "docker"
+    ]; # aggiungi "docker" se lo usi
     shell = pkgs.zsh;
 
     #initialPassword = "nixos";
     hashedPassword = "$6$1zqJzMI1UgQinQug$sp1uVndN9MN/.azhIavPWRamncv4JHCgRKRhcCYNCMYhCnClGMYnxCn.JbVPSUs8HfduKYDKSyDTAlnd8czcd.";
 
     openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW7C8X/k4K9qmbvrOWorpDz0v1lPcvBTA9psCtWIOtQ emoriver@live.it"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW7C8X/k4K9qmbvrOWorpDz0v1lPcvBTA9psCtWIOtQ emoriver@live.it"
     ];
   };
 }
