af98d8a56d23298a4562452b042985211d91f643
emoriver <emoriver@live.it>
2026-06-29 16:24:28 +0200

    provo a installare i temi scuri su node-red

--- modules/nixos/users/carpinera.nix
+++ modules/nixos/users/carpinera.nix
@@ -20,11 +20,11 @@
     initialPassword = "carpinera";
 
     # ssh-keygen -t ed25519 -C "emoriver@__nome host__"
-    #openssh.authorizedKeys.keys = [
+    openssh.authorizedKeys.keys = [
     #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW7C8X/k4K9qmbvrOWorpDz0v1lPcvBTA9psCtWIOtQ emoriver@live.it"
 
-    #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAR56Aa22c+Q+Uj+7V7skB6ZldUnfvbxy2dqZIAXbH4D emoriver@barronera"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAR56Aa22c+Q+Uj+7V7skB6ZldUnfvbxy2dqZIAXbH4D emoriver@barronera"
     #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/23fWiczmReYRwPaKRwv5dhzZhQWqXgl89OOOxW7G8 emoriver@p16s1"
-    #];
+    ];
   };
 }
